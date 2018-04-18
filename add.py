258c638e292f7713d1c3c4670ba3e04c25e6efb0
RIYA JOSE <50116@fisat>
2018-04-18 01:18:59 -0400

    created function to add 4 numbers

--- add.py
+++ add.py
@@ -1,4 +1,4 @@
-def add(num1,num2):
+def add(num1,num2)
 	s=num1+num2
 	return s
 print add(2,3)
@@ -6,3 +6,8 @@ def add3(num1,num2,num3):
 	s=num1+num2+num3
 	return s
 print add3(2,3,4)
+def add4(n,m,l,o):
+	s=n+m+l+o
+	return s
+print add4(2,3,4,5)
+
